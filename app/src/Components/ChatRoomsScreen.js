b896d23075ffaf470f5f3ebb2653c046ae815bca
Miguel teheran <miguelteheran@gmail.com>
2020-10-21 18:52:56 -0500

    adding user login and first screen
    (cherry picked from commit 28b6e5a537f760da4562515835b2234587311ebd)

--- app/src/Components/ChatRoomsScreen.js
+++ app/src/Components/ChatRoomsScreen.js
@@ -1,55 +1,9 @@
 import React from 'react'
-import 'ed-grid'
 
 export default function ChatRoomsScreen() {
-    /*
-    const [channels, chatInfo] = useState('');
-
-    const getChannels = () => {
-        
-    }
-
-    const getChatInfo = () => {
-        
-    }
-    */
     return (
-        <div class="ed-container chat">
-            <div class="s-10 chat-menu">
-                <button class="chat-menu__option">Channel1</button>
-                <button class="chat-menu__option">Channel1</button>
-                <button class="chat-menu__option">Channel1</button>
-                <button class="chat-menu__option">Channel1</button>
-                <button class="chat-menu__option">Channel1</button>
-                <button class="chat-menu__option">Channel1</button>
-                <button class="chat-menu__option">Channel1</button>
-                <button class="chat-menu__option">Channel1</button>
-            </div>
-            <div class="ed-item s-90">
-                <div class="chat-info">
-                    <div class="chat-info__window">
-                        <span class="chat-info__user">Mi nombre es blablabla</span>
-                        <p class="chat-info__info">blablablablablablablablablabla</p>
-                    </div>
-                    <div class="chat-info__window">
-                        <span class="chat-info__user">Mi nombre es blablabla</span>
-                        <p class="chat-info__info">blablablablablablablablablabla</p>
-                    </div>
-                    <div class="chat-info__window">
-                        <span class="chat-info__user">Mi nombre es blablabla</span>
-                        <p class="chat-info__info">blablablablablablablablablabla</p>
-                    </div>
-                    <div class="chat-info__window">
-                        <span class="chat-info__user">Mi nombre es blablabla</span>
-                        <p class="chat-info__info">blablablablablablablablablabla</p>
-                    </div>
-                    <div class="chat-info__window">
-                        <span class="chat-info__user">Mi nombre es blablabla</span>
-                        <p class="chat-info__info">blablablablablablablablablabla</p>
-                    </div>
-                </div>
-                <textarea class="chat-text" placeholder="write a message..."></textarea>
-            </div>
+        <div>
+            Bienvenido a las salas de chat
         </div>
     )
 }
